aeb4fe989e9019496b766a613717d37b8d79bc3b
robin2017 <robin.seu@foxmail.com>
2020-08-18 00:20:23 +0800

    准备debug

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,9 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    let query = ctx.query;
     ctx.body = 'hi, egg';
+    console.log('query:', JSON.stringify(query))
   }
 }
 
